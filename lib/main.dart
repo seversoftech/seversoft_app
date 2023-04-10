b5f8f31f112af69a473afa9c22cdecf7e9eb246c
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-10 15:00:39 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'page.dart';
-
+import 'package:flutter/services.dart';
+import 'navbar.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark),
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Seversoft Tech',
-      theme: ThemeData(
-        
-      ),
-      home: const Homepage(),
-      
+      theme: ThemeData(),
+      home: NavBarHome(),
     );
   }
 }
