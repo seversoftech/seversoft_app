9362d1a2cb739f60e8d8b196c10c4275bbe18f7d
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-10 15:02:42 +0100

    Bottom Nav Added

---
+++ lib/navbar.dart
@@ -0,0 +1,130 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:seversoftapp/screens/electricity.dart';
+import 'package:seversoftapp/screens/hompage.dart';
+import 'screens/airtime.dart';
+import 'screens/cable.dart';
+import 'screens/data.dart';
+
+class NavBarHome extends StatefulWidget {
+  const NavBarHome({Key? key}) : super(key: key);
+
+  @override
+  State<NavBarHome> createState() => _NavBarHomeState();
+}
+
+class _NavBarHomeState extends State<NavBarHome> {
+  final _pageController = PageController(initialPage: 2);
+
+  int maxCount = 5;
+
+  // widget list
+  final List<Widget> bottomBarPages = [
+    Data(),
+    Airtime(),
+    Hompage(),
+    Cable(),
+    Electricity(),
+  ];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            bottomBarPages.length, (index) => bottomBarPages[index]),
+      ),
+      extendBody: true,
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              showBlurBottomBar: false,
+              pageController: _pageController,
+              color: Colors.white,
+              showLabel: true,
+              notchColor: Color(0xFF08062C),
+              bottomBarItems: [
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.podcasts,
+                    color: Color(0xFF08062C),
+                  ),
+                  activeItem: Icon(
+                    Icons.podcasts_outlined,
+                    color: Colors.greenAccent,
+                  ),
+                  itemLabel: 'Data',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.sim_card,
+                    color: Color(0xFF08062C),
+                  ),
+                  activeItem: Icon(
+                    Icons.sim_card,
+                    color: Colors.blueAccent,
+                  ),
+                  itemLabel: 'Airtime',
+                ),
+
+                ///svg example
+                BottomBarItem(
+                  inActiveItem: SvgPicture.asset(
+                    'assets/icons/home.svg',
+                    color: Color(0xFF08062C),
+                  ),
+                  activeItem: SvgPicture.asset(
+                    'assets/icons/home.svg',
+                    color: Colors.white,
+                  ),
+                  itemLabel: 'Home',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.tv,
+                    color: Color(0xFF08062C),
+                  ),
+                  activeItem: Icon(
+                    Icons.tv,
+                    color: Colors.pink,
+                  ),
+                  itemLabel: 'Cable Tv',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.electric_bolt,
+                    color: Color(0xFF08062C),
+                  ),
+                  activeItem: Icon(
+                    Icons.electric_bolt,
+                    color: Colors.yellow,
+                  ),
+                  itemLabel: 'Electricity',
+                ),
+              ],
+              onTap: (index) {
+                /// control your animation using page controller
+                _pageController.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.bounceIn,
+                );
+              },
+            )
+          : null,
+    );
+  }
+}
---
+++ lib/screens/epins.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Epins extends StatefulWidget {
+  @override
+  EpinsState createState() => EpinsState();
+}
+
+class EpinsState extends State<Epins> {
+  late WebViewController _controller;
+  double progress = 0;
+
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: false,
+        body: RefreshIndicator(
+            onRefresh: () {
+              return (_controller.reload());
+            },
+            child: willMethod(context)),
+      ),
+    );
+  }
+
+  WillPopScope willMethod(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _goBack(context),
+      child: Column(
+        children: [
+          LinearProgressIndicator(
+            minHeight: 1,
+            color: Color.fromARGB(225, 79, 70, 229),
+            backgroundColor: Colors.white,
+            value: progress,
+          ),
+          Expanded(
+            child: WebView(
+              initialUrl: "https://app.seversoftech.com/vpaccount/?vend=epins",
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controllerCompleter.future
+                    .then((value) => _controller = value);
+                _controllerCompleter.complete(webViewController);
+              },
+              onProgress: (progress) =>
+                  setState(() => this.progress = progress / 100),
+              onWebResourceError: (WebResourceError error) {
+                setState(
+                  () {
+                    _loadHtmlFromAssets();
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+//GO BACK IN WEBVIEW
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text('Are you serious?'),
+                actions: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      SystemNavigator.pop();
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
+              ));
+      return Future.value(true);
+    }
+  }
+
+// RELOAD WEBVIEW
+  reloadWebView() {
+    _controller.reload();
+    SnackTest.show(context, 'Reloading...');
+  }
+
+// LOAD ERROR HTML PAGE
+  _loadHtmlFromAssets() async {
+    String fileText = await rootBundle.loadString('assets/error.html');
+    _controller.loadUrl(Uri.dataFromString(
+      fileText,
+      mimeType: 'text/html',
+      encoding: Encoding.getByName('utf-8'),
+    ).toString());
+  }
+}
+
+// SNACKBAR TEXT CLASS
+class SnackTest {
+  final String message;
+
+  const SnackTest({required this.message});
+
+  static show(
+    BuildContext context,
+    String message,
+  ) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        elevation: 5,
+        //behavior: SnacBarBehavior.floating,
+        content: Text(message),
+        duration: new Duration(seconds: 2),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(225, 79, 70, 229),
+      ),
+    );
+  }
+}
