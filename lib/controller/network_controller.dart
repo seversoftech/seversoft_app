37ebf38d59fe3686d939ddec85355d7e72511fc7
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-12 06:45:56 +0100

    Minor Changes

--- lib/controller/network_controller.dart
+++ lib/controller/network_controller.dart
@@ -5,34 +5,39 @@ import 'package:get/get.dart';
 class NetworkController extends GetxController {
   final Connectivity _connectivity = Connectivity();
 
-  @override 
+  @override
   void onInit() {
     super.onInit();
     _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
   void _updateConnectionStatus(ConnectivityResult connectivityResult) {
-      
-      if (connectivityResult == ConnectivityResult.none) {
-        Get.rawSnackbar(
-          messageText: const Text(
-            'PLEASE CONNECT TO THE INTERNET',
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.rawSnackbar(
+          titleText: Text(
+            ' Cannot connect to network.',
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 14
-            )
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Color(0xFF090735)),
           ),
+          messageText: const Text(
+              'There was an error connecting to the network. Please check your internet connection and try again. If the problem persists, please contact your network administrator for assistance.',
+              style: TextStyle(color: Colors.white, fontSize: 14)),
           isDismissible: false,
           duration: const Duration(days: 1),
-          backgroundColor: Colors.red[400]!,
-          icon : const Icon(Icons.wifi_off, color: Colors.white, size: 35,),
+          backgroundColor: Color.fromARGB(255, 161, 13, 10)!,
+          icon: const Icon(
+            Icons.settings_input_antenna,
+            color: Colors.white,
+            size: 35,
+          ),
           margin: EdgeInsets.zero,
-          snackStyle: SnackStyle.GROUNDED
-        );
-      } else {
-        if (Get.isSnackbarOpen) {
-          Get.closeCurrentSnackbar();
-        }
+          snackStyle: SnackStyle.GROUNDED);
+    } else {
+      if (Get.isSnackbarOpen) {
+        Get.closeCurrentSnackbar();
       }
+    }
   }
 }
