2cc2970b0c9afeef5de3cf28fe3972630ae651ee
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-13 05:15:34 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,15 @@ void main() async {
     sound: true,
   );
   print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
   runApp(MyApp());
   DependencyInjection.init();
 }
