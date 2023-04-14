ce4d2f3adea8c26be108498ea5395669d895faff
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-14 16:01:55 +0100

    ...

--- lib/controller/network_controller.dart
+++ lib/controller/network_controller.dart
@@ -15,30 +15,30 @@ class NetworkController extends GetxController {
   void _updateConnectionStatus(ConnectivityResult connectivityResult) {
     if (connectivityResult == ConnectivityResult.none) {
       Get.rawSnackbar(
-          snackPosition: SnackPosition.TOP,
-          titleText: Text(
-            ' Cannot connect to network.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Color(0xFF090735)),
-          ),
-          messageText: const Text(
-              'There was an error connecting to the network. Please check your internet connection and try again. If the problem persists, please contact your network administrator for assistance.',
-              style: TextStyle(color: Colors.white, fontSize: 24)),
-          isDismissible: false,
-          duration: const Duration(days: 1),
-          backgroundColor: Color.fromARGB(255, 230, 15, 8),
-          // icon: SvgPicture.asset(
-          //   'assets/icons/no_signal.svg',
-          //   height: 500,
-          //   width: 500,
-          //   allowDrawingOutsideViewBox: true,
-          // ),
-          margin: EdgeInsets.zero,
-          snackStyle: SnackStyle.GROUNDED,
-          mainButton: ElevatedButton(onPressed: () {}, child: Text('Retry')));
+        snackPosition: SnackPosition.TOP,
+        titleText: Text(
+          ' Cannot connect to network.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              color: Color(0xFF090735)),
+        ),
+        messageText: const Text(
+            'There was an error connecting to the network. Please check your internet connection and try again. If the problem persists, please contact your network administrator for assistance.',
+            style: TextStyle(color: Colors.white, fontSize: 24)),
+        isDismissible: false,
+        duration: const Duration(days: 1),
+        backgroundColor: Color.fromARGB(255, 230, 15, 8),
+        // icon: SvgPicture.asset(
+        //   'assets/icons/no_signal.svg',
+        //   height: 500,
+        //   width: 500,
+        //   allowDrawingOutsideViewBox: true,
+        // ),
+        margin: EdgeInsets.zero,
+        snackStyle: SnackStyle.GROUNDED,
+      );
     } else {
       if (Get.isSnackbarOpen) {
         Get.closeCurrentSnackbar();
