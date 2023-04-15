5a7ba61a8407f948594d21e550cd887cb9e72f52
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-04-15 05:38:34 +0100

    ...

--- lib/controller/network_controller.dart
+++ lib/controller/network_controller.dart
@@ -1,7 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:full_screen_image/full_screen_image.dart';
 import 'package:get/get.dart';
 
 class NetworkController extends GetxController {
